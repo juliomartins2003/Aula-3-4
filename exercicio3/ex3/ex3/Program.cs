22ac97873dffb6ab4baf6557cd0bcdf8934073ed
juliomartins2003 <juliomhomartins@gmail.com>
2022-02-23 17:17:06 +0000

    ex3

---
+++ exercicio3/ex3/ex3/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Ana Ruivo".PadLeft(20) + "Porto".PadLeft(10));
+Console.WriteLine("Joaquina Pinto".PadLeft(20) + "Aveiro".PadLeft(10));
+Console.WriteLine("Miguel Costa".PadLeft(20) + "Braga".PadLeft(10));
