7f082927c6a936ae504fb2f8a872d2fbe5d2a8ac
juliomartins2003 <juliomhomartins@gmail.com>
2022-02-23 17:14:56 +0000

    ex2

---
+++ exercicio2/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,9 @@
+﻿string nome = "alunos";
+string msg = "Bem-vindos";
+//1 METODO
+Console.WriteLine("Caros {0} sejam {1} ao C#!", nome, msg);
+
+
+
+//2 METODO
+Console.WriteLine("Caros " + nome + " sejam " + msg + " ao C#!");
