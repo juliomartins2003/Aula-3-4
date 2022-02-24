8c5c8d2401edb08c80088a13b79e0035cd5b930c
juliomartins2003 <juliomhomartins@gmail.com>
2022-02-24 16:56:38 +0000

    ex8

--- exercicio6/ex6/ex6/Program.cs
+++ exercicio6/ex6/ex6/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("3^5 = {0}", Math.Pow(3, 5));
 Console.WriteLine("raiz quadrada 125 = {0}", Math.Sqrt(125));
-Console.WriteLine("raiz quadrada 125 = {0}", Math.Round(Math.Sqrt(125),0));
-Console.WriteLine("raiz quadrada 125 = {0}", Math.Sin(30));
+Console.WriteLine("Arredondamento com 0 casas decimais" = {0}", Math.Round(Math.Sqrt(125),0));
+Console.WriteLine(" Seno de 30 = {0}", Math.Sin(30));
 
 
---
+++ exercicio8/ex8/ex8/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("4==5 : {0}", 4==5);
+
+bool x = 4 == 5;
+Console.WriteLine("4==5 : " + x);
+Console.WriteLine("4!=5 : {0}", 4 != 5);
+Console.WriteLine("4>5 : {0}", 4 > 5);
+Console.WriteLine("4<5 e 6>10 : {0}", 4 < 5 && 6 > 10);
+Console.WriteLine("40<50 ou 60>90 : {0}", 40 < 50 || 60 > 90);
+Console.WriteLine(" não 40<50 ou 60>90 : {0}", !(40 < 50 || 60 > 90));
