4756fc0582c6a0541a9f9083e2c84e6f75297c04
juliomartins2003 <juliomhomartins@gmail.com>
2022-02-24 15:47:48 +0000

    ex7

---
+++ exercicio6/ex6/ex6/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("3^5 = {0}", Math.Pow(3, 5));
+Console.WriteLine("raiz quadrada 125 = {0}", Math.Sqrt(125));
+Console.WriteLine("raiz quadrada 125 = {0}", Math.Round(Math.Sqrt(125),0));
+Console.WriteLine("raiz quadrada 125 = {0}", Math.Sin(30));
+
+
